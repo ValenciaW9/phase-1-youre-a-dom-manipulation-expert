e0183d54f2c7f8f8455479e6ffd65793a9714d15
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-15 14:43:34 -0400

    phase-1-youre-a-dom-manipulation-expert/
    phase-1-youre-a-dom-manipulation-expert/

---
+++ phase-1-lecture-videos-dom-events-main/indexTest.js
@@ -0,0 +1,16 @@
+
+const { /* import any necessary functions or elements */ } = require('../index');
+
+// Describe the test suite
+describe('Index Tests', () => {
+  // Test case to check if dodger moves to the right on "ArrowRight" key press
+  it('should move the dodger to the right on "ArrowRight" key press', () => {
+    // Setup any necessary elements or variables
+
+    // Simulate the keydown event with "ArrowRight" key
+
+    // Check if the dodger element's position has been updated correctly
+  });
+});
+
+// Run the tests using a testing framework or test runner
