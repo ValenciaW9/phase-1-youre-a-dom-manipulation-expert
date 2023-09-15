6a60a96a705ec2dc5ab1979714d1767f6af14918
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-15 16:08:21 -0400

    phase-1-youre-a-dom-manipulation-expert/
    phase-1-youre-a-dom-manipulation-expert/

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,21 @@
-const chai = require("chai");
-global.expect = chai.expect;
-const jsdom = require("mocha-jsdom");
+const chai = require('chai')
+global.expect = chai.expect
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require("@babel/core");
+const url = "http://localhost"
 
-jsdom({});
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+
+const babelResult = babel.transformFileSync(
+  path.resolve(__dirname, '..', 'index.js'), {
+    presets: ['@babel/env']
+  }
+);
+
+const src = babelResult.code
+
+jsdom({
+  html, src, url
+});
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,12 +1,46 @@
-// The code below ensures that students who are using CodeGrade will get credit
-// for the code-along in Canvas; you can disregard it.
+require ( './helpers.js' );
 
-require("./helpers.js");
+describe('moveDodgerLeft()', () => {
+  beforeEach(() => {
+    dodger = document.getElementById('dodger')
+  })
 
-describe("", () => {
-  describe("", () => {
-    it("Test passing", () => {
-      return true;
-    });
-  });
-});
+  it('moves the DODGER to the left', () => {
+    let left = dodger.style.left
+    left = parseInt(left)
+
+    moveDodgerLeft()
+
+    let newPosition = dodger.style.left
+    newPosition = parseInt(newPosition)
+
+    if (left > 0){
+      expect(newPosition).to.be.below(left)
+    } else {
+      expect(newPosition).to.equal(left)
+    }
+  })
+})
+
+describe('moveDodgerRight()', () => {
+  beforeEach(() => {
+    dodger = document.getElementById('dodger')
+  })
+
+  it('moves the DODGER to the right', () => {
+    let left = dodger.style.left
+    left = parseInt(left)
+
+    moveDodgerRight()
+
+    let newPosition = dodger.style.left
+    newPosition = parseInt(newPosition)
+    
+    if (left < 360){
+      expect(newPosition).to.be.above(left)
+    } else{
+      expect(newPosition).to.equal(left)
+    }
+    
+  })
+})
